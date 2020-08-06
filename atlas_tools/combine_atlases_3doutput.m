e3d525ad7879637b4cea59147deafedfb3410796
Scott Burwell <scott.j.burwell@gmail.com>
2020-08-06 12:04:11 -0500

    updated help in combine_atlas_* scripts

--- atlas_tools/combine_atlases_3doutput.m
+++ atlas_tools/combine_atlases_3doutput.m
@@ -1,24 +1,21 @@
-% Use this script to 
+% Use this script to combine *multiple* atlas files (3d nii where each integer value >0 reflecting one homogeneous anatomical region of interest)
+% and save out to one 3d nii file. Requires SPM12 and the group ICA toolbox (i.e., GIFT) be in the user's path already.
 %
-%
-%
-%
+% Example:
+% >> outatlas = './Yeo17+HarvardOxfordSubcortical.nii'; % name for nii file to be output
+% >> atlases  = { ...
+%    './lead-dbs/templates/space/MNI_ICBM_2009b_NLIN_ASYM/labeling/Yeo2011_17Networks_MNI152 (Yeo 2011).nii', ...              %atlas containing cortical regions
+%    './lead-dbs/templates/space/MNI_ICBM_2009b_NLIN_ASYM/labeling/Harvard Oxford Thr25 2mm Whole Brain (Makris 2006).nii' ... %atlas including subcortical regions
+%    };
+%    rois = { ...
+%      'all', ... %extract all parcels from first atlas
+%    [92:105] ... %extract only certain regions from second atlas
+%    };
+% >> combine_atlases_3doutput; %run it!  
 %
 % Requires: 
 % SPM12: spm_vol, spm_read_vols, spm_write_vol
 % GroupICATv4.0a/icatb (GIFT): icatb_resizeData
-
-
-outatlas = '/labs/burwellstudy/data/rois/Yeo17+HarvardOxfordSubcortical.nii';
-atlases  = { ...
-'./lead-dbs/templates/space/MNI_ICBM_2009b_NLIN_ASYM/labeling/Yeo2011_17Networks_MNI152 (Yeo 2011).nii', ...              %atlas containing cortical regions
-'./lead-dbs/templates/space/MNI_ICBM_2009b_NLIN_ASYM/labeling/Harvard Oxford Thr25 2mm Whole Brain (Makris 2006).nii' ... %atlas including subcortical regions
-};
-rois = { ...
-  'all', ... %extract all parcels from first atlas
-[92:105] ... %extract only certain regions from second atlas
-};
-
 
 counter = 0;
 for ii  = 1:length(atlases), 
